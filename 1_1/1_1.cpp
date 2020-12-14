ff91aca1ead3e37b962220b1ffdd2a9d93e3c53a
mvideo <mvideo@LAPTOP-TO4NDI3I>
2020-12-14 20:40:24 +0500

    readme

--- 1_1/1_1.cpp
+++ 1_1/1_1.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main() {
+int main1_1() {
     int max, a, b, c; 
     cin >> a >> b >> c;
 
---
+++ 1_3/1_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    if n < 5 {
+        n = n * 3:
+    } else {
+        if n > 7 {
+            n = n + 3;
+        } else {
+            n = n % 10
+        }
+    }
+
+    cout << "n = " << n;
+
+    return 0;
+}
